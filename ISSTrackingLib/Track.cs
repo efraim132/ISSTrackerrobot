8b3f3a5364bc11437c990c0e9b583ab8edda9059
Efraim Grebnev <efraim.egrebnev.grebnev605@gmail.com>
2018-08-24 08:33:40 -0700

    Add project files.

---
+++ ISSTrackingLib/Track.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ISSTracking
+{
+    public struct Location {
+        public float lon;
+        public float lat;
+    }
+
+    public class Track {
+        public static Location GetLocation() {
+            var RawResponse = new WebClient().DownloadString("http://api.open-notify.org/iss-now.json");
+
+            dynamic Response = JsonConvert.DeserializeObject(RawResponse);
+            Location location = new Location();
+            location.lat = Response.iss_position.latitude;
+            location.lon = Response.iss_position.longitude;
+            return location;
+        } 
+    }
+
+}
